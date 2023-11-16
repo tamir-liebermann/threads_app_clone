f37dd040b69e58aa219eb6524c28449bf200376d
tamir-liebermann <tamirlieb2@gmail.com>
2023-11-16 19:33:32 +0200

    vercel error fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
